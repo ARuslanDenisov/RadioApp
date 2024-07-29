289be890ce7d8c49ddb19d5c14fcf0ad82d277cb
KrasnovYuri <krasnov1yuri@gmail.com>
2024-07-29 14:22:11 +0300

    fix

--- RadioApp/AuthViews/AuthView.swift
+++ RadioApp/AuthViews/AuthView.swift
@@ -7,45 +7,4 @@
 
 import SwiftUI
 
-struct AuthView: View {
-    @StateObject private var viewModel = SignInEmailViewModel()
-    
-    var body: some View {
-        VStack {
-            TextField("Your email", text: $viewModel.email)
-                .padding()
-                .background(.background)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(lineWidth: 1)
-                        .foregroundColor(.red)
-                )
-            
-            SecureField("Your password", text: $viewModel.password)
-                .padding()
-                .background(.background)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 5)
-                        .stroke(lineWidth: 1)
-                        .foregroundColor(.red)
-                )
-            
-            Button {
-                viewModel.signIn()
-            } label: {
-                Text("Sign In")
-                    .font(.headline)
-                    .frame(height: 55)
-                    .frame(maxWidth: .infinity)
-                    .foregroundColor(.white)
-                    .background(.mint)
-            }
-        }
-        .padding()
-        .navigationTitle("Sign In With Email")
-    }
-}
 
-#Preview {
-    AuthView()
-}
