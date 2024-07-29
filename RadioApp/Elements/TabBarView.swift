f987e1229d180d9bd86700b2f26f4d6479a75f99
SnakCat <snakkdss@gmail.com>
2024-07-29 20:53:14 +0300

    -fix tab bar

--- RadioApp/Elements/TabBarView.swift
+++ RadioApp/Elements/TabBarView.swift
@@ -9,58 +9,56 @@ import SwiftUI
 
 struct TabBarView: View {
     
-    @State private var selectedTab = 0
+    @Binding var selectedTab: Int
     
     var body: some View {
         HStack {
-            Spacer()
+          Spacer()
             // tab Popular View
-            Button {
-                self.selectedTab = 0
-            } label: {
                 VStack {
                     Text("Popular")
-                        .font(.system(size: 20))
+                        .font(.custom(FontApp.regular, size: 20))
                         .foregroundStyle(selectedTab == 0 ? .white : .gray)
                     Circle()
                         .frame(width: 15, height: 15)
                         .foregroundStyle(selectedTab == 0 ? .raLightBlue : .clear)
                 }
-            }
+                .onTapGesture {
+                    selectedTab = 0
+                }
             Spacer()
             // tab Favorites View
-            Button {
-                self.selectedTab = 1
-            } label: {
                 VStack {
                     Text("Favorites")
-                        .font(.system(size: 20))
+                        .font(.custom(FontApp.regular, size: 20))
                         .foregroundStyle(selectedTab == 1 ? .white : .gray)
                     Circle()
                         .frame(width: 15, height: 15)
                         .foregroundStyle(selectedTab == 1 ? .raLightBlue : .clear)
                 }
-            }
+                .onTapGesture {
+                    selectedTab = 1
+                }
             Spacer()
             // tab AllStaions View
-            Button {
-                self.selectedTab = 2
-            } label: {
                 VStack {
                     Text("AllStations")
-                        .font(.system(size: 20))
+                        .font(.custom(FontApp.regular, size: 20))
                         .foregroundStyle(selectedTab == 2 ? .white : .gray)
                     Circle()
                         .frame(width: 15, height: 15)
                         .foregroundStyle(selectedTab == 2 ? .raLightBlue : .clear)
                 }
-            }
+                .onTapGesture {
+                    selectedTab = 2
+                }
             Spacer()
         }
         .background(Color("RADarkBlue"))
+        .animation(.easeInOut, value: selectedTab)
     }
 }
 
 #Preview {
-    TabBarView()
+    TabBarView(selectedTab: .constant(1))
 }
