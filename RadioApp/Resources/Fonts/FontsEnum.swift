79021f36578e1789cec871a2483bea62ca725c7f
ARuslanDenisov <angess47@gmail.com>
2024-07-29 19:31:54 +0700

    04 authView

--- RadioApp/Resources/Fonts/FontsEnum.swift
+++ RadioApp/Resources/Fonts/FontsEnum.swift
@@ -8,14 +8,14 @@
 import Foundation
 
 
-enum Font {
-    static let black = "SF-Pro-Display-Black"
-    static let bold = "SF-Pro-Display-Bold"
-    static let heavy = "SF-Pro-Display-Heavy"
-    static let light = "SF-Pro-Display-Light"
-    static let medium = "SF-Pro-Display-Medium"
-    static let regular = "SF-Pro-Display-Regular"
-    static let semiBold = "SF-Pro-Display-Semibold"
-    static let thin = "SF-Pro-Display-Thin"
-    static let ultraLight = "SF-Pro-Display-Ultralight"
+enum FontApp {
+    static let black = "SFProDisplay-Black"
+    static let bold = "SFProDisplay-Bold"
+    static let heavy = "SFProDisplay-Heavy"
+    static let light = "SFProDisplay-Light"
+    static let medium = "SFProDisplay-Medium"
+    static let regular = "SFProDisplay-Regular"
+    static let semiBold = "SFProDisplay-Semibold"
+    static let thin = "SFProDisplay-Thin"
+    static let ultraLight = "SFProDisplay-Ultralight"
 }
