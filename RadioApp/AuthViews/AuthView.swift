532eb811b516eb577825b41be342c82be401c24b
ARuslanDenisov <angess47@gmail.com>
2024-07-29 13:54:14 +0700

    04_1

--- RadioApp/AuthViews/AuthView.swift
+++ RadioApp/AuthViews/AuthView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct AuthView: View {
+    var signIn: Bool = true
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("bgAuth")
+                .resizableToFill()
+                .ignoresSafeArea()
+                
+            VStack {
+                HStack {
+                    Image("appLogo")
+                        .resizableToFit()
+                        .frame(width: 58, height: 58)
+                    Spacer()
+                }
+                VStack {
+                    Text(signIn ? "Sign in" : "Sign up")
+                        .foregroundStyle(.white)
+                        .font(.custom(Font.light, size: 50))
+                }
+            }
+        }
     }
 }
 
---
+++ RadioApp/Resources/Fonts/FontsEnum.swift
@@ -0,0 +1,21 @@
+//
+//  FontsEnum.swift
+//  RadioApp
+//
+//  Created by Руслан on 29.07.2024.
+//
+
+import Foundation
+
+
+enum Font {
+    static let black = "SF-Pro-Display-Black"
+    static let bold = "SF-Pro-Display-Bold"
+    static let heavy = "SF-Pro-Display-Heavy"
+    static let light = "SF-Pro-Display-Light"
+    static let medium = "SF-Pro-Display-Medium"
+    static let regular = "SF-Pro-Display-Regular"
+    static let semiBold = "SF-Pro-Display-Semibold"
+    static let thin = "SF-Pro-Display-Thin"
+    static let ultraLight = "SF-Pro-Display-Ultralight"
+}
