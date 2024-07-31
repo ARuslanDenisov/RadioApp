768763e548f9ccd26f726c5640684943150fc6f5
ARuslanDenisov <angess47@gmail.com>
2024-07-31 12:47:11 +0700

    15_add some navigation

--- RadioApp/AllStationView/AllStationView.swift
+++ RadioApp/AllStationView/AllStationView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AllStationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.raDarkBlue
+                .ignoresSafeArea()
+            Text("All station view")
+                .foregroundStyle(.white)
+        }
     }
 }
 
--- RadioApp/RootView.swift
+++ RadioApp/RootView.swift
@@ -12,30 +12,84 @@ struct RootView: View {
     @StateObject var dataViewModel = DataViewModel()
     @State var index = 0
     
-    
     var body: some View {
         
+        ZStack {
+            Color.raDarkBlue
+                .ignoresSafeArea()
             NavigationView {
-                ZStack {
-                    VStack {
-                        Spacer()
-                        TabBarView(selectedTab: $index)
-                    }
+                switch index {
+                case 0: PopularView()
+                case 1: FavoriteView(viewModel: dataViewModel)
+                case 2: AllStationView()
+                default: PopularView()
                 }
-            .onAppear {
-//                let authUser = try? FBAuthService.shared.getAuthenticationUser()
-//                self.showAuthView = authUser == nil
+
             }
-            .fullScreenCover(isPresented: $showAuthView, content: {
-                NavigationView {
-                    AuthView(mainViewModel: dataViewModel, showAuthView: $showAuthView)
+            .navigationViewStyle(.stack)
+            
+            VStack {
+                //header
+                VStack {
+                    HStack(spacing: 0) {
+                        Image("appLogo")
+                            .resizableToFit()
+                            .frame(width: 33)
+                            .padding(.trailing, 7)
+                        Text("Hello, ")
+                            .foregroundStyle(.white)
+                            .font(.custom(FontApp.bold, size: 30))
+                        Text(dataViewModel.user.name.isEmpty ? "New user" : dataViewModel.user.name)
+                            .foregroundStyle(.raPink)
+                            .font(.custom(FontApp.bold, size: 30))
+                        Spacer()
+                        NavigationLink {
+                            ProfileView()
+                        } label: {
+                            //тут будет картинка пользователя
+                            Image("")
+                                .resizableToFit()
+                            ZStack {
+                                Rectangle()
+                                    .foregroundStyle(.white)
+                            }
+                            .clipShape(TriangleShape().offset(x:-20, y: 15))
+                            
+                            
+                        }
+                        .frame(width: 30,height: 30)
+                        
+                    }
+                    .padding(5)
+                    Spacer()
                 }
-            })
+                //                    .padding(.top, 60)
+                Spacer()
+                //tabbar
+                TabBarView(selectedTab: $index)
+            }
+            
         }
-        .padding()
+        //animation
+        
+        .animation(.easeInOut(duration: 1), value: index)
+        
+        .onAppear {
+            let authUser = try? FBAuthService.shared.getAuthenticationUser()
+            self.showAuthView = authUser == nil
+        }
+        .fullScreenCover(isPresented: $showAuthView, content: {
+            NavigationView {
+                AuthView(mainViewModel: dataViewModel, showAuthView: $showAuthView)
+            }
+        })
+        
+        
     }
 }
 
 #Preview {
-    RootView()
+    NavigationView {
+        RootView()
+    }
 }
