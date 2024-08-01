09a9b6af49864a7c40d7ec3dfb33444dba3101bf
KrasnovYuri <krasnov1yuri@gmail.com>
2024-08-01 12:02:47 +0300

    add RadioBigAllStationElement

--- RadioApp/AllStationView/AllStationView.swift
+++ RadioApp/AllStationView/AllStationView.swift
@@ -8,16 +8,84 @@
 import SwiftUI
 
 struct AllStationView: View {
+
+    var dataVM: DataViewModel
+    var stationModel: [StationModel]
+    @State var searchRadio: String = ""
+    
     var body: some View {
         ZStack {
             Color.raDarkBlue
                 .ignoresSafeArea()
-            Text("All station view")
-                .foregroundStyle(.white)
+            
+            VStack {
+                    Text("All Stations")
+                        .foregroundStyle(.white)
+                        .font(.custom(FontApp.regular, size: 30))
+                    .padding(.top, 100)
+                
+                SearchField(searchRadio: $searchRadio)
+                
+                ScrollView(showsIndicators: false) {
+                    VStack(spacing: 12) {
+                        ForEach(stationModel, id: \.id) { station in
+                            RadioBigAllStationElement(dataVM: dataVM, playingNow: false)
+                        }
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
+
+extension View {
+    func placeholder<Content: View>(
+        when shouldShow: Bool,
+        alignment: Alignment = .leading,
+        @ViewBuilder placeholder: () -> Content) -> some View {
+
+        ZStack(alignment: alignment) {
+            placeholder().opacity(shouldShow ? 1 : 0)
+            self
         }
     }
 }
 
 #Preview {
-    AllStationView()
+    AllStationView(dataVM: DataViewModel(user: UserModel(id: "", name: "Mark", email: "", photoUrl: "", favorites: []), stationNow: StationModel()), stationModel: [StationModel(id: "", name: "Arizona", favicon: "", streamUrl: "", tags: "", language: "", countryCode: "", votes: 1)])
+}
+
+struct SearchField: View {
+    @Binding var searchRadio: String
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .frame(height: 44)
+                .foregroundStyle(.raMediumBlue)
+            
+            HStack {
+                Image(systemName: "magnifyingglass")
+                    .foregroundStyle(.white)
+                    .padding()
+                TextField("Search radio station", text: $searchRadio)
+                    .foregroundStyle(.white)
+                    .placeholder(when: searchRadio.isEmpty) {
+                        Text("Search radio station").foregroundColor(.white)
+                    }
+                Button {
+                    
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .padding(9)
+                        .foregroundStyle(.raLightBlue)
+                        .background(.black)
+                        .clipShape(Circle())
+                        .padding()
+                }
+            }
+        }
+    }
 }
--- RadioApp/Elements/RadioBigAllStationElement.swift
+++ RadioApp/Elements/RadioBigAllStationElement.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct RadioBigAllStationElement: View {
+    var dataVM: DataViewModel
+    @State var playingNow: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 15)
+                .foregroundStyle(playingNow ? .raPink : .raDarkBlue )
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(.raLightGray)
+            
+            HStack {
+                Spacer()
+                VStack {
+                    Text("votes \(dataVM.stationNow.votes)")
+                        .foregroundStyle(.white)
+                        .font(.custom(FontApp.bold, size: 10))
+                    Spacer()
+                }
+            }
+            .padding(10)
+            
+            VStack(alignment: .leading) {
+                Text(dataVM.stationNow.name)
+                    .foregroundStyle(.white)
+                    .font(.custom(FontApp.bold, size: 30))
+                
+                HStack {
+                    Text(dataVM.stationNow.tags.isEmpty ? "Popular" : "\(dataVM.stationNow.tags)")
+                        .foregroundStyle(.white)
+                    .font(.custom(FontApp.regular, size: 15))
+                    Spacer()
+                    ZStack{
+                        Color.blue
+                    }
+                    .frame(width: 94, height: 23)
+                }
+                
+                Text(playingNow ? "Playing now" : "")
+                    .foregroundStyle(.gray)
+                    .opacity(0.8)
+                    .font(.custom(FontApp.bold, size: 14))
+            }
+            .padding(.horizontal, 22)
+        }
+        .frame(width: 293, height: 123)
     }
 }
 
 #Preview {
-    RadioBigAllStationElement()
+    RadioBigAllStationElement(dataVM: DataViewModel(user: UserModel(id: "", name: "Mark", email: "", photoUrl: "", favorites: []), stationNow: StationModel()), playingNow: true)
 }
