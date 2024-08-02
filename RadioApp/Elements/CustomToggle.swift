a47660efae85ddb3684c1940e2c1acbc851a06ff
KrasnovYuri <krasnov1yuri@gmail.com>
2024-08-02 22:22:35 +0300

    add CustomToggle

--- RadioApp/Elements/CustomToggle.swift
+++ RadioApp/Elements/CustomToggle.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
-struct CustomToggle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+struct CustomToggle: ToggleStyle {
+    var onColor: Color
+       var offColor: Color
+       var thumbColor: Color
 
-#Preview {
-    CustomToggle()
-}
+       func makeBody(configuration: Self.Configuration) -> some View {
+           HStack {
+               configuration.label
+                   .font(.body)
+               Spacer()
+               RoundedRectangle(cornerRadius: 16, style: .circular)
+                   .fill(configuration.isOn ? onColor : offColor)
+                   .frame(width: 50, height: 30)
+                   .overlay(
+                       Circle()
+                           .fill(thumbColor)
+                           .padding(2)
+                           .offset(x: configuration.isOn ? 10 : -10)
+                   )
+                   .onTapGesture {
+                       withAnimation(.smooth(duration: 0.2)) {
+                           configuration.isOn.toggle()
+                       }
+                   }
+           }
+       }
+   }
+
