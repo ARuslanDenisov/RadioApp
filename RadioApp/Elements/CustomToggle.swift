53ef5bb2ee5067624944d48887a416ee34e8cd65
KrasnovYuri <krasnov1yuri@gmail.com>
2024-08-02 22:22:12 +0300

    add CustomToggle

---
+++ RadioApp/Elements/CustomToggle.swift
@@ -0,0 +1,18 @@
+//
+//  CustomToggle.swift
+//  RadioApp
+//
+//  Created by Юрий on 02.08.2024.
+//
+
+import SwiftUI
+
+struct CustomToggle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomToggle()
+}
